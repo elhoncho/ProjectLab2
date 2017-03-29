b5d89f9e5c8c23b1db2fc06354182e0132be7dc0
karken <teknist@gmail.com>
2017-03-29 16:11:00 -0500

    Wifi Driver
    Added the wifi driver stuff

---
+++ Wifi/stringtools.h
@@ -0,0 +1,14 @@
+/*
+ * StringTools.h
+ *
+ *  Created on: Mar 25, 2017
+ *      Author: Owner
+ */
+
+#ifndef STRINGTOOLS_H_
+#define STRINGTOOLS_H_
+
+int endsWith (char* base, const char* str);
+void trimString(char *str);
+
+#endif /* STRINGTOOLS_H_ */
