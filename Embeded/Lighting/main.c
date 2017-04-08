5e802675efa9147322dec3b073a0e6e23b8ad7db
karken <teknist@gmail.com>
2017-04-07 23:58:07 -0500

    Updated
    Updated the timer variable, I was off by a factor of 10. Pushing work in
    progress on lighting

--- Embeded/Lighting/main.c
+++ Embeded/Lighting/main.c
@@ -6,6 +6,17 @@
 /*
  * main.c
  */
+
+#define IDLE 0
+#define DELAY 1
+#define TRIGGER 2
+
+volatile int state = 0;
+int delayTime = 2;
+int onTime = 2;
+volatile long timer = 0;
+volatile long lastTime = 0;
+
 int main(void) {
     long timer = 0;
 
@@ -13,7 +24,7 @@ int main(void) {
 
       P1OUT &= 0x00;               // Shut down everything
       P1DIR &= 0x00;
-      P1DIR |= BIT0+BIT5;            // P1.0 and P1.6 pins output the rest are input
+      P1DIR |= BIT0+BIT5;            // P1.0 and P1.5 pins output the rest are input
       P1REN |= BIT4;                   // Enable internal pull-up/down resistors
       P1OUT |= BIT4;                   //Select pull-up mode for P1.4
       P1IE |= BIT4;                       // P1.4 interrupt enabled
@@ -24,12 +35,29 @@ int main(void) {
 
     while(1){
         WifiLoop();
-        if(NewData() == 0){
-            if(strncmp(rxData, "LI|", 3) == 0){
-                char tmpStr[9] = "LI|0";
-                tmpStr[3] = appliance;
-                SendData(tmpStr);
-            }
+//        if(NewData() == 0){
+//            if(strncmp(rxData, "LI|", 3) == 0){
+//                char tmpStr[9] = "LI|0";
+//                tmpStr[3] = appliance;
+//                SendData(tmpStr);
+//            }
+//        }
+        switch(state){
+            case IDLE:
+                break;
+            case DELAY:
+                if(timer >= TimeSinceBoot() + delayTime){
+                    state = TRIGGER;
+                    timer = TimeSinceBoot();
+                    P1OUT |= BIT5;
+                }
+                break;
+            case TRIGGER:
+                if(timer >= TimeSinceBoot() + onTime){
+                    state = IDLE;
+                    P1OUT &= ~BIT5;
+                }
+                break;
         }
     }
     return 0;
@@ -37,6 +65,11 @@ int main(void) {
 
 void __attribute__((interrupt(PORT1_VECTOR))) Port_1(void)
 {
-   P1OUT ^= BIT0+BIT5;
+    lastTime = timer;
+    timer = TimeSinceBoot();
+    state = DELAY;
+    P1OUT &= ~BIT5;
+
+   P1OUT ^= BIT0;
    P1IFG &=~BIT4;                        // P1.3 IFG cleared
 }
--- Embeded/Motion Detector/main.c
+++ Embeded/Motion Detector/main.c
@@ -2,42 +2,67 @@
 #include <wifi.h>
 #include <string.h>
 
-#define RedLed  (0x01u)    //%00000001 O port pin position P1.0
-#define mDetect (0x10u)    //%00001000 I port pin position P1.4
-#define GreenLed (0x40u)   //%01000000 O port pin position P1.6
+#define RedLed  BIT0    //%00000001 O port pin position P1.0
+#define mDetect BIT4    //%00001000 I port pin position P1.4
+#define GreenLed BIT6   //%01000000 O port pin position P1.6
+
+
+#define IDLE 0
+#define SEND_ALARM 1
+#define ALARM_SENT 2
+#define SEND_CLEAR 3
+
+volatile int state = IDLE;
 
 int main(void) {
+    P1DIR |= RedLed + GreenLed;  //1.0, 1.6 as outputs
+    P1OUT &= 0x00;               // Shut down everything
+    P1DIR &= 0x00;
+    P1REN |= BIT4;               // Enable internal pull-up/down resistors
+    P1OUT |= BIT4;                   //Select pull-up mode for P1.4
+    P1IE |= BIT4;                       // P1.4 interrupt enabled
+    P1IES &= ~BIT4;                     // P1.4 rising edge
+    P1IFG &= ~BIT4;                  // P1.4 IFG cleared
+
+
     WifiSetup();
-    P1DIR |= (RedLed+GreenLed); //1.0, 1.6 as outputs
-	//P1REN = (mDetect);          //resister enable for P1.4
-	P1OUT &= ~(RedLed+GreenLed); //set to 0
-	//P1IE |= (mDetect);           //interrupt enabled
-	//P1IES |=(mDetect);           //hi/lo edge
-	//P1IFG &= ~(mDetect);         //P1.4 IFG cleared
-	//__enable_interrupt();
-	while(1)
+
+    while(1)
 	{
 	      WifiLoop();
-	      if((mDetect & P1IN)  ==(0x10u))  //Case MDetect ON
-	      {
-	            P1OUT |= (GreenLed); // Toggle Green LED ON
-	          //  txData="MD|1"
-	      }
-	      else //Case OFF
-	      {
-	            P1OUT &= ~ (GreenLed); // Toggle Green LED OFF
-	          //  txData="MD|0"
-	      }
-	      //SendData(txData);
 
+	      switch(state){
+	          case IDLE:
+
+	          break;
+	          case SEND_ALARM:
+	              if(SendData("MD|1") == 0){
+	                  state = ALARM_SENT;
+	              }
+	              break;
+	          case ALARM_SENT:
+	              break;
+	          case SEND_CLEAR:
+	              if(SendData("MD|0") == 0){
+	                  state = IDLE;
+	              }
+              break;
+	      }
 	}
 	return 0;
 }
-/*
-void __attribute__((interrupt(PORT1_VECTOR))) PORT_1(void)
+
+void __attribute__((interrupt(PORT1_VECTOR))) Port_1(void)
 {
-    P1OUT ^=(RedLed);
-    P1IFG &= ~ (mDetect);
-   // txData="MD|1";
+   P1OUT ^= BIT0;
+   if(state == IDLE){
+       state = SEND_ALARM;
+       P1IES |= BIT4;
+   }
+   else if(state == ALARM_SENT){
+       state = SEND_CLEAR;
+       P1IES &= ~BIT4;
+   }
+
+   P1IFG &=~BIT4;                        // P1.3 IFG cleared
 }
-*/
