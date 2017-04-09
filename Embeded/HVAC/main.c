b7d13e3450623fa0f4aacdf5a715fc1f140ffa60
karken <teknist@gmail.com>
2017-04-08 19:37:27 -0500

    update

--- Embeded/HVAC/main.c
+++ Embeded/HVAC/main.c
@@ -14,7 +14,7 @@ int main(void) {
     char cooling = '0';
     char fan = '0';
 
-    P1DIR |= BIT3+BIT4+BIT5;
+    P2DIR |= BIT3+BIT4+BIT5;
     P1OUT = 0;
 
     while(1){
@@ -23,29 +23,29 @@ int main(void) {
             if(strncmp(rxData, "AC|", 3) == 0){
                 if(rxData[3] == '0'){
                     heating = rxData[3];
-                    P1OUT &= ~BIT3;
+                    P2OUT &= ~BIT3;
                 }
                 else if(rxData[3] == '1'){
                     heating = rxData[3];
-                    P1OUT |= BIT3;
+                    P2OUT |= BIT3;
                 }
 
                 if(rxData[5] == '0'){
                     cooling = rxData[5];
-                    P1OUT &= ~BIT4;
+                    P2OUT &= ~BIT4;
                 }
                 else if(rxData[5] == '1'){
                     cooling = rxData[5];
-                    P1OUT |= BIT4;
+                    P2OUT |= BIT4;
                 }
 
                 if(rxData[7] == '0'){
                     fan = rxData[7];
-                    P1OUT &= ~BIT5;
+                    P2OUT &= ~BIT5;
                 }
                 else if(rxData[7] == '1'){
                     fan = rxData[7];
-                    P1OUT |= BIT5;
+                    P2OUT |= BIT5;
                 }
 
                 char tmpStr[9] = "AC|0|0|0";
