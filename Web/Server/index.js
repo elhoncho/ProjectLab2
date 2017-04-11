33ddc9d370932aff59e3f43d2151bf2a4a870832
karken <teknist@gmail.com>
2017-04-10 21:18:12 -0500

    Out with the old
    Deleted the old web pages and changed the port to 80 for our site

--- Web/Server/index.js
+++ Web/Server/index.js
@@ -23,8 +23,8 @@ res.sendFile(__dirname + '/index.html');
 });
 
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(80, function(){
+  console.log('listening on *:80');
 });
 
 io.on('connection', function(socket){
