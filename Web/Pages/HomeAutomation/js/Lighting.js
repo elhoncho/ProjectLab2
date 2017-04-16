4aac0f6279ba44128b119332cbaf658cccb74ce8
ElHoncho <tekinst@gmail.com>
2017-04-15 23:40:52 -0500

    Web Update
    Updated the website, dosent spam on the slider anymore. Formting fixes.

--- Web/Pages/HomeAutomation/js/Lighting.js
+++ Web/Pages/HomeAutomation/js/Lighting.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
 	var socket = io();
 	var appliance = "0";
+    var lastTxTime = 0;
+    var txDelay = 500;
+    var sliderUpdate = true;
 
     $("#low").click(function(){
-	socket.emit('Lighting', "LI|65");
+	   if(Date.now() - lastTxTime > txDelay){
+            socket.emit('Lighting', "LI|65");
+            lastTxTime = Date.now();
+        }
     });
 
     $("#high").click(function(){
-	socket.emit('Lighting', "LI|0");
+        if(Date.now() - lastTxTime > txDelay){
+            socket.emit('Lighting', "LI|0");
+            lastTxTime = Date.now();
+        }
+	
     }); 
 
     $('.no-zoom').bind('touchend', function(e) {
@@ -17,12 +27,28 @@ $(document).ready(function() {
 
     $(function(){
         $("#slider").slider({
-	    min: 0,
-	    max: 65,
-	    slide: function(event, ui){
-                socket.emit('Lighting', "LI|"+ui.value);
-            }
-	});
+    	    min: 0,
+    	    max: 65,
+    	    slide: function(event, ui){
+                sliderUpdate = true;
+                if(Date.now() - lastTxTime > txDelay){
+                    socket.emit('Lighting', "LI|"+ui.value);
+                    lastTxTime = Date.now();
+                    sliderUpdate = false;
+                }
+            }   
+	   });
     });
+
+
+
+    window.setInterval(function(){
+        if(sliderUpdate){
+            socket.emit('Lighting', "LI|"+$("#slider").slider("value"));
+            lastTxTime = Date.now();
+            sliderUpdate = false;
+        }
+    }, 500);
+
 
 });
