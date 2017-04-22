93471eeef2fa64fa98954dd3b49d33df5fd62ac8
elhoncho <teknist@gmail.com>
2017-04-22 17:25:30 -0500

    Web Updates
    Web Updates

--- Web/Pages/HomeAutomation/js/HVAC.js
+++ Web/Pages/HomeAutomation/js/HVAC.js
@@ -4,13 +4,19 @@ var heating = "0";
 var cooling = "0";
 var fan = "0";
 
+$('#toggle-heating').bootstrapToggle('off');
+$('#toggle-cooling').bootstrapToggle('off');
+$('#toggle-fan').bootstrapToggle('off');
+
     $("#heating").click(function(){
     if(heating == "0"){
     heating = "1";
+    $('#toggle-heating').bootstrapToggle('on');
     socket.emit('HVAC', "AC|"+heating+"|"+cooling+"|"+fan);
     }
     else{
     heating = "0";
+    $('#toggle-heating').bootstrapToggle('off');
     socket.emit('HVAC', "AC|"+heating+"|"+cooling+"|"+fan);
     }
     });
@@ -18,10 +24,12 @@ var fan = "0";
     $("#cooling").click(function(){
     if(cooling == "0"){
     cooling = "1";
+    $('#toggle-cooling').bootstrapToggle('on');
     socket.emit('HVAC', "AC|"+heating+"|"+cooling+"|"+fan);
     }
     else{
     cooling = "0";
+    $('#toggle-cooling').bootstrapToggle('off');
     socket.emit('HVAC', "AC|"+heating+"|"+cooling+"|"+fan);
     }
     }); 
@@ -29,17 +37,19 @@ var fan = "0";
     $("#fan").click(function(){
     if(fan == "0"){
     fan = "1";
+    $('#toggle-fan').bootstrapToggle('on');
     socket.emit('HVAC', "AC|"+heating+"|"+cooling+"|"+fan);
     }
     else{
     fan = "0";
+    $('#toggle-fan').bootstrapToggle('off');
     socket.emit('HVAC', "AC|"+heating+"|"+cooling+"|"+fan);
     }
     });  
 
     $('.no-zoom').bind('touchend', function(e) {
- e.preventDefault();
-  $(this).click();
-})
+        e.preventDefault();
+        $(this).click();
+    })
 
 });
